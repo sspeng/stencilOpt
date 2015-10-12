ba73d926c46b69269bb02e1b6871ead3884d0306
xiszishu <xiszishu@gmail.com>
2015-10-11 21:25:01 -0700

    update10/11

--- jacobi7_opt.c
+++ jacobi7_opt.c
@@ -7,14 +7,39 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #ifndef SIZE
 #define SIZE 500
 #endif
 
 volatile int log_ready = 0;
-double log_data[SIZE*SIZE*SIZE];
+int p1index,p2index,logtime=0;
+double log_data[SIZE*SIZE*SIZE],pmem1[SIZE*SIZE*SIZE+1],pmem2[SIZE*SIZE*SIZE+1];
 pthread_t *log_id = 0;
+
+static inline void asm_clflush(volatile intptr_t *addr)
+{
+    __asm__ __volatile__ ("clflush %0 "::"m"(*addr));
+}
+static inline void asm_mfence(void)
+{
+    __asm__ __volatile__ ("mfence");
+}
+static inline uint64_t  rdtsc()
+{
+    unsigned long a,d;
+    __asm__ __volatile__ ("cpuid; rdtsc":"=a"(a),"=d"(d)::"ebx","ecx");
+    return a | ((uint64_t)d<<32);
+}
+
+void flush(double *pmem,unsigned size)
+{
+    int i,j;
+    for (i=0;i<=size;i++)
+    asm_clflush(&pmem[i]);
+    asm_mfence();
+}
 
 void log_all_data(unsigned *size)
 {
@@ -24,17 +49,33 @@ void log_all_data(unsigned *size)
   for (;;) {
     while (!log_ready); 
     logindex = (logindex + 1)%2;
-    sprintf(logname, "log_%d",logindex);
-    FILE* logfile=fopen(logname, "w");
-    fwrite(&log_ready, sizeof(log_ready), 1, logfile);
-    fwrite(log_data, sizeof(double), *size, logfile);
-    fclose(logfile);
+    //sprintf(logname, "log_%d",logindex);
+    //FILE* logfile=fopen(logname, "w");
+    //fwrite(&log_ready, sizeof(log_ready), 1, logfile);
+    //fwrite(log_data, sizeof(double), *size, logfile);
+    //fclose(logfile);
+
+    if (logindex)
+    {
+        memcpy((void*)pmem1,(void*)log_data,(*size) * sizeof(double));
+        pmem1[(*size)]=log_ready;
+        //flush(pmem1,*size);
+    }
+    else
+    {
+        memcpy((void*)pmem2,(void*)log_data,(*size) * sizeof(double));
+        pmem2[(*size)]=log_ready;
+        //flush(pmem2,*size);
+    }
+    logtime++;
     log_ready = 0;
   }
 }
 
 int recover_data(double* data, unsigned size)
 {
+    int res;
+    /*
   FILE* logfile0=fopen("log_0", "r");
   FILE* logfile1=fopen("log_1", "r");
   if (logfile0==0 || logfile1 == 0) return 0;
@@ -51,6 +92,18 @@ int recover_data(double* data, unsigned size)
   } 
   fclose(logfile0);
   fclose(logfile1);
+    */
+
+  if (p1index < p2index)
+  {
+      memcpy((void*)data,(void*)pmem2,size * sizeof(double));
+      res = p1index;
+  } 
+  else
+  {
+      memcpy((void*)data,(void*)pmem1,size * sizeof(double));
+      res = p2index;
+  }
   return res;
 }
 
@@ -72,7 +125,8 @@ void jacobi7_3(const int nx,const int ny, int nz, const double alpha,double* A0,
     pthread_create(log_id,NULL, log_all_data, &size); 
   }
 
-  /*@;BEGIN(Nest1=Nest)@*/for (t = tstart; t < timesteps; t++) {
+  /*@;BEGIN(Nest1=Nest)@*/for (t = tstart; t < timesteps; t++)
+      {
           int do_log = !(log_ready);
           if (t%2 == 0) { l0 = A0; lnext = Anext; }
           else {lnext = A0; l0 = Anext; }
@@ -95,5 +149,6 @@ void jacobi7_3(const int nx,const int ny, int nz, const double alpha,double* A0,
        log_ready=t+1;
     }
   }
+  printf("logtime: %d\n",logtime);
 }
 
